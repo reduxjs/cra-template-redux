6c6c49bb82b66d3daef1df5aee3cfd9c1b3f0f26
Ben <benlorantfy@gmail.com>
2020-01-19 00:40:28 -0500

    Change to CSS modules

--- template/src/features/counter/index.js
+++ template/src/features/counter/index.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { decrement, increment, selectCount } from './counterSlice';
-import './counter.css';
+import styles from './counter.module.css';
 
 export function Counter() {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
 
   return (
-    <p className="Counter-container">
+    <p className={styles.container}>
       <button
-        className="Counter-button"
+        className={styles.button}
         aria-label="Increment value"
         onClick={() => dispatch(increment())}
       >
         +
       </button>
-      <span className="Counter-value">{count}</span>
+      <span className={styles.value}>{count}</span>
       <button
-        className="Counter-button"
+        className={styles.button}
         aria-label="Decrement value"
         onClick={() => dispatch(decrement())}
       >
