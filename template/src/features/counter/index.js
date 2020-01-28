58776a01153aefe69021c53837ea28c847022f21
Ben <benlorantfy@gmail.com>
2020-01-27 22:40:42 -0500

    Implement code review suggestions

--- template/src/features/counter/index.js
+++ template/src/features/counter/index.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { decrement, increment, selectCount } from './counterSlice';
-import styles from './counter.module.css';
+import { decrement, increment, incrementByAmount, selectCount } from './counterSlice';
+import styles from './Counter.module.css';
 
 export function Counter() {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState(2);
 
   return (
-    <p className={styles.container}>
-      <button
-        className={styles.button}
-        aria-label="Increment value"
-        onClick={() => dispatch(increment())}
-      >
-        +
-      </button>
-      <span className={styles.value}>{count}</span>
-      <button
-        className={styles.button}
-        aria-label="Decrement value"
-        onClick={() => dispatch(decrement())}
-      >
-        -
-      </button>
-    </p>
+    <div>
+      <div className={styles.container}>
+        <button
+          className={styles.button}
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          +
+        </button>
+        <span className={styles.value}>{count}</span>
+        <button
+          className={styles.button}
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          -
+        </button>
+      </div>
+      <div className={styles.container}>
+        <input className={styles.textbox} value={incrementAmount} onChange={(e) => setIncrementAmount(Number(e.target.value))} />
+        <button 
+          className={styles.button}
+          onClick={() => dispatch(incrementByAmount({ amount: incrementAmount }))}
+        >
+          Add Amount
+        </button>
+      </div>
+    
+    </div>
   );
 }
