08fc6ee8e1b3e3f9d5a3f2acbede02592c8669a9
Ben <benlorantfy@gmail.com>
2020-03-09 15:22:34 -0400

    Allow `incrementAsync` to take an argument and update layout

--- template/src/features/counter/counterSlice.js
+++ template/src/features/counter/counterSlice.js
@@ -22,17 +22,21 @@ export const slice = createSlice({
   },
 });
 
+export const { increment, decrement, incrementByAmount } = slice.actions;
+
+// The function below is called a thunk and allows us to perform async logic. It
+// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
+// will call the thunk with the `dispatch` function as the first argument. Async
+// code can then be executed and other actions can be dispatched
+export const incrementAsync = amount => dispatch => {
+  setTimeout(() => {
+    dispatch(incrementByAmount(amount));
+  }, 1000);
+};
+
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state) => state.counter.value)`
 export const selectCount = state => state.counter.value;
-
-export const { increment, decrement, incrementByAmount } = slice.actions;
-
-export const incrementAsync = () => dispatch => {
-  setTimeout(() => {
-    dispatch(increment());
-  }, 1000);
-};
 
 export default slice.reducer;
