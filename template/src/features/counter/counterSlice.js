677f2e3237274a83dc215ca16f753ea3705c1106
Mark Erikson <mark@isquaredsoftware.com>
2020-03-13 19:03:52 -0400

    Update template/src/features/counter/counterSlice.js
    Co-Authored-By: Nick McCurdy <nick@nickmccurdy.com>

--- template/src/features/counter/counterSlice.js
+++ template/src/features/counter/counterSlice.js
@@ -8,7 +8,7 @@ export const slice = createSlice({
   reducers: {
     increment: state => {
       // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the inner library,
+      // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
       state.value += 1;
