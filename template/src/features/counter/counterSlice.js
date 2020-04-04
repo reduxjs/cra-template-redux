e0cca2198026b1f614d13d7013078486ac30a56c
Mark Erikson <mark@isquaredsoftware.com>
2020-04-04 15:06:09 -0400

    Clarify counter slice name

--- template/src/features/counter/counterSlice.js
+++ template/src/features/counter/counterSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const slice = createSlice({
+export const counterSlice = createSlice({
   name: 'counter',
   initialState: {
     value: 0,
@@ -22,7 +22,7 @@ export const slice = createSlice({
   },
 });
 
-export const { increment, decrement, incrementByAmount } = slice.actions;
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 
 // The function below is called a thunk and allows us to perform async logic. It
 // can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
@@ -39,4 +39,4 @@ export const incrementAsync = amount => dispatch => {
 // in the slice file. For example: `useSelector((state) => state.counter.value)`
 export const selectCount = state => state.counter.value;
 
-export default slice.reducer;
+export default counterSlice.reducer;
