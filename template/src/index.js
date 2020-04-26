232e45098e164944667fdf84b26424852ad6dbec
Shorsh <jorge@cliengo.com>
2020-04-26 01:16:41 -0300

    Add styled-component base

--- template/src/index.js
+++ template/src/index.js
@@ -4,12 +4,32 @@ import './index.css';
 import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 import * as serviceWorker from './serviceWorker';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Montserrat', sans-serif;
+    box-sizing: border-box;
+    background-color: ${({ theme }) => theme.backgroundColor};
+    margin:0;
+    min-height: 100vh;
+  }
+`;
+
+const theme = {
+  backgroundColor: '#F6F8FC',
+  primary: '#8DC9BA',
+  secondary: 'white',
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
