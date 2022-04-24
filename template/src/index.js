faee85557664d84e79f29f30003133a831078305
Mark Erikson <mark@isquaredsoftware.com>
2022-04-24 15:57:53 -0400

    Include ESLint config from CRA template and tweak improt ordering

--- template/src/index.js
+++ template/src/index.js
@@ -1,11 +1,10 @@
-import './index.css';
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
